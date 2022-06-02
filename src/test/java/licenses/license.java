1aa70363188eeceea9fa667ab5bdfca176cdc194
Uday <udayesh@gmail.com>
2022-06-02 07:24:57 -0400

    2nd commit

--- src/test/java/licenses/license.java
+++ src/test/java/licenses/license.java
@@ -6,26 +6,37 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class license extends TestBase{
     @Test
 
     public void addLicense() throws InterruptedException {
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.login("admin", "admin123", true, null);
+        loginPage.login("Admin", "admin123", true, null);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+
         driver.findElement(By.xpath("//*[@id=\"menu_admin_viewAdminModule\"]/b")).click();
-        Thread.sleep(1000);
+
         driver.findElement(By.xpath("//a[@id='menu_admin_Qualifications']")).click();
-        Thread.sleep(1000);
+
         driver.findElement(By.xpath("//a[@id='menu_admin_viewLicenses']")).click();
-        Thread.sleep(1000);
+
         driver.findElement(By.xpath("//input[@id='btnAdd']")).click();
+
+        // adding new license
 
         WebElement textBox;
         textBox = driver.findElement(By.name("license[name]"));
         textBox.sendKeys("Project Management Professional (PMP)");
 
         driver.findElement(By.name("btnSave")).click();
-        driver.close();
+
+        //delete an existing license
+
+        driver.findElement(By.xpath("//*[@id=\"recordsListTable\"]/tbody/tr[7]/td[1]/input")).click();
+        driver.findElement(By.id("btnDel")).click();
+
 
 
 
